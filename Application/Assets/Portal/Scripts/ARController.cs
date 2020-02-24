135e8f2424889e6db2ddbb3bb2471ebd045fe0ec
Kaloyan Marshalov <kaloyan.marshalov@gmail.com>
2020-02-24 13:35:32 +0000

    Added door on raycast hit

--- Application/Assets/Portal/Scripts/ARController.cs
+++ Application/Assets/Portal/Scripts/ARController.cs
@@ -8,7 +8,9 @@ public class ARController : MonoBehaviour
 {
     /*Planes detected from the current frame*/
     private List<DetectedPlane> newTrackedPlanes = new List<DetectedPlane>();
-    public GameObject gridPrefab;
+    public GameObject GridPrefab;
+    public GameObject Door;
+    public GameObject ARCamera;
 
     // Start is called before the first frame update
     void Start()
@@ -29,9 +31,31 @@ public class ARController : MonoBehaviour
 
         for(int i = 0; i < newTrackedPlanes.Count; i++)
         {
-            GameObject grid = Instantiate(gridPrefab, Vector3.zero, Quaternion.identity, transform);
+            GameObject grid = Instantiate(GridPrefab, Vector3.zero, Quaternion.identity, transform);
 
             grid.GetComponent<DetectedPlaneVisualizer>().Initialize(newTrackedPlanes[i]);
+        }
+
+        Touch touch;
+        // No interaction
+        if(Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        TrackableHit hit;
+        if(Frame.Raycast(touch.position.x, touch.position.y, TrackableHitFlags.PlaneWithinPolygon, out hit))
+        {
+            Door.SetActive(true);
+            Anchor anchor = hit.Trackable.CreateAnchor(hit.Pose);
+            Door.transform.position = hit.Pose.position;
+            Door.transform.rotation = hit.Pose.rotation;
+
+            Vector3 cameraPosition = ARCamera.transform.position;
+            cameraPosition.y = hit.Pose.position.y;
+            Door.transform.LookAt(cameraPosition, Door.transform.up);
+
+            Door.transform.parent = anchor.transform;
         }
     }
 }
