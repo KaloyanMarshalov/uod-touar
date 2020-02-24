8197ec1ea675d3d070a4b2ef0cb42934591802b5
Kaloyan Marshalov <kaloyan.marshalov@gmail.com>
2020-02-24 20:57:17 +0000

    Working portal to Sponza

---
+++ Application/Assets/Portal/Scripts/DoorManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DoorManager : MonoBehaviour
+{
+    public GameObject mainCamera;
+
+    public GameObject Sponza;
+
+    private Material[] SponzaMaterials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SponzaMaterials = Sponza.GetComponent<Renderer>().sharedMaterials;
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay(Collider collider)
+    {
+        Vector3 cameraPositionInDoorSpace = transform.InverseTransformPoint(mainCamera.transform.position);
+
+        if(cameraPositionInDoorSpace.y < 0.5f)
+        {
+            for (int i = 0; i < SponzaMaterials.Length; i++)
+            {
+                SponzaMaterials[i].SetInt("_StencilComp", (int)CompareFunction.Always);
+            }
+        } 
+        else
+        {
+            for (int i = 0; i < SponzaMaterials.Length; i++)
+            {
+                SponzaMaterials[i].SetInt("_StencilComp", (int)CompareFunction.Equal);
+            }
+        }
+    }
+}
