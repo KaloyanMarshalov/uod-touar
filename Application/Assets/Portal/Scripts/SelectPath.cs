1cc17b8fe713625eb736410a82974427102b86c2
Kaloyan Marshalov <kaloyan.marshalov@gmail.com>
2020-03-06 23:00:36 +0000

    Button toggle and positioning on frame spawner

--- Application/Assets/Portal/Scripts/SelectPath.cs
+++ Application/Assets/Portal/Scripts/SelectPath.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SelectPath : MonoBehaviour
 {
@@ -35,16 +37,32 @@ public class SelectPath : MonoBehaviour
     {
         int numberOfObjects = 5;
         float radius = 8f;
+        GameObject button = GameObject.Find(EventSystem.current.currentSelectedGameObject.name);
 
-        for (int i = 0; i < numberOfObjects; i++)
+        GameObject container = GameObject.Find("FrameContainer");
+        if (container)
         {
-            float angle = i * Mathf.PI * 2 / numberOfObjects;
-            float x = Mathf.Cos(angle) * radius;
-            float z = Mathf.Sin(angle) * radius;
-            Vector3 pos = ARCamera.transform.position + new Vector3(x, 0, z);
-            float angleDegrees = -angle * Mathf.Rad2Deg;
-            Quaternion rot = Quaternion.Euler(0, angleDegrees, 0);
-            Instantiate(PictureFrame, pos, rot);
+            Object.Destroy(container);
+            button.GetComponentInChildren<Text>().text = "Show Paths";
+        }
+        else 
+        {
+            GameObject frameContainer = new GameObject("FrameContainer");
+            button.GetComponentInChildren<Text>().text = "Hide Paths";
+
+            for (int i = 0; i < numberOfObjects; i++)
+            {
+                float angle = i * Mathf.PI * 2 / numberOfObjects;
+                float x = Mathf.Cos(angle) * radius;
+                float z = Mathf.Sin(angle) * radius;
+                Vector3 pos = ARCamera.transform.position + new Vector3(x, 0, z);
+                float angleDegrees = -angle * Mathf.Rad2Deg;
+                Quaternion rot = Quaternion.Euler(0, angleDegrees, 0);
+                GameObject frame = Instantiate(PictureFrame, pos, rot) as GameObject;
+                frame.transform.parent = frameContainer.transform;
+                //Change shader image
+                //Change text above image
+            }
         }
     }
 }
