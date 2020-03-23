fa061eadcfcdc9d5644f0c2c9c65f66b664b6475
Kaloyan Marshalov <kaloyan.marshalov@gmail.com>
2020-03-23 16:52:54 +0000

    Text above mapbox pin

---
+++ Application/Assets/Portal/Scripts/RotatePinText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePinText : MonoBehaviour
+{
+    [SerializeField]
+    GameObject text;
+
+    void OnMouseDown()
+    {
+        text.transform.localRotation = Camera.main.transform.localRotation;
+        text.GetComponent<TextMesh>().text = gameObject.transform.parent.name;
+    }
+}
